8d409fb06705b5139e5ecb4a9fdc35e0cc9b9fb6
spacetime-timespace <alvin.gao.2020@gmail.com>
2025-08-10 21:06:29 -0700

    Update python.py

--- python.py
+++ python.py
@@ -6,6 +6,9 @@ WIDTH = 200
 HEIGHT = 200
 FPS = 120
 
+x = 0
+y = 0
+
 class RGBWindow(arcade.Window):
     def __init__(self):
         super().__init__(WIDTH, HEIGHT, "RGB Animation", update_rate=1/FPS)
@@ -23,12 +26,25 @@ class RGBWindow(arcade.Window):
         img[:, :, 1] = (np.cos(t) * 31 + 224).astype(np.uint8)  # Green
         img[:, :, 2] = ((np.sin(t * 0.5) + 1) * 31 + 224).astype(np.uint8)  # Blue
 
+        img[x:x+20, y:y+20, :] = 0
+
         # Convert to Arcade texture and draw
         texture = arcade.Texture(name="rgb_frame", image=arcade.ImageData(WIDTH, HEIGHT, img))
         texture.draw_scaled(WIDTH//2, HEIGHT//2, 1, 1)
 
     def on_key_press(self,symbol,modifiers):
-        
+        if symbol = arcade.key.LEFT:
+            if x > 0:
+                x -= 1
+        if symbol = arcade.key.RIGHT:
+            if x < 180:
+                x += 1  
+        if symbol = arcade.key.UP:
+            if y < 180:
+                x += 1
+        if symbol = arcade.key.DOWN:
+            if y > 0:
+                x -= 1
 
 if __name__ == "__main__":
     RGBWindow()
