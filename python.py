22775d39cca681dd3b52aa94bcf1eba7b227742b
fenl <145521130+TenderGalaxy@users.noreply.github.com>
2025-08-10 20:54:04 -0700

    Update python.py

--- python.py
+++ python.py
@@ -4,24 +4,19 @@ import numpy as np
 import time
 
 width, height = 200, 200
-fps = 30
+fps = 120
 delay = 1 / fps
 
 while True:
-    # Create a changing RGB image (moving color gradient)
     t = time.time()
     img = np.zeros((height, width, 3), dtype=np.uint8)
-    img[:, :, 0] = (np.sin(t) * 127 + 128).astype(np.uint8)  # Blue channel
-    img[:, :, 1] = (np.cos(t) * 127 + 128).astype(np.uint8)  # Green channel
-    img[:, :, 2] = ((np.sin(t * 0.5) + 1) * 127).astype(np.uint8)  # Red channel
-
-    # Show image
-    cv2.imshow("RGB Animation", img)
-
-    # Exit on 'q'
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    for i in range(len(img)):
+        for j in range(len(img)):
+            img[i][j] = np.sin(i/height) + np.sin(j/width) + np.sin((time.time()/5) % (2*np.pi) )
+    cv2.imshow("gyatt", img)
+    if keyboard.is_pressed('x')
         break
 
-    time.sleep(delay)  # Keep ~30 FPS
+    time.sleep(delay)
 
 cv2.destroyAllWindows()
