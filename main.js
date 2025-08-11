859e4515c3890c24e117e3451a706d20179c2e0f
fenl <145521130+TenderGalaxy@users.noreply.github.com>
2025-08-10 20:42:40 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
+console.log('Hello World')
 
+for(let i = 0; i < 3; i++){
+  console.log(i)
+}
+
+i = {name: "Bob", age: "42"}
+console.log(i.name,i.age)
+
+class Skibidis{
+  constructor(level,rizz){
+    this.level = level
+    this.rizz = rizz
+  }
+  toString(){
+    return   `Skibidi Level ${this.level} with aura level ${this.rizz}`
+  }
+}
+
+alex = new Skibidi(1000,999)
+console.log(alex.toString())
