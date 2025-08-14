58568d00db0764b2e4c74d1af729de936322824e
spacetime-timespace <alvin.gao.2020@gmail.com>
2025-08-13 17:53:42 -0700

    Update python.py

--- python.py
+++ python.py
@@ -13,14 +13,23 @@ class Game(arcade.Window):
         super().__init__(WIDTH, HEIGHT, "RGB Animation", update_rate=1/FPS)
         self.start_time = time.time()
         self.tex = arcade.Texture(image = Image.fromarray(np.zeros((HEIGHT,WIDTH,4),dtype = np.uint8)))
-        self.sp = arcade.Sprite()
-        self.sp.center_x = 100
-        self.sp.center_y = 100
-        arcade.start_render()
-        self.spl = arcade.SpriteList()
-        self.spl.append(self.sp)
         self.x = 0
         self.y = 0
+        t = time.time() - self.start_time
+        self.img = np.zeros((HEIGHT, WIDTH, 4), dtype=np.uint8) # Arcade needs RGBA
+        self.img[:, :, 0] = (np.sin(t) * 31 + 224).astype(np.uint8)
+        self.img[:, :, 1] = (np.cos(t) * 31 + 224).astype(np.uint8)
+        self.img[:, :, 2] = (np.sin(t * 0.5) * 31 + 224).astype(np.uint8)
+        self.img[:, :, 3] = 255
+
+        self.img[self.x:self.x+20, self.y:self.y+20, :3] = 0
+
+        self.im = Image.fromarray(self.img)
+        self.sp = arcade.Sprite(self.im,1)
+        self.sp.center_x = 100
+        self.sp.center_y = 100
+        self.spl = arcade.SpriteList()
+        self.spl.append(self.sp)
 
     def on_draw(self):
 
@@ -37,6 +46,7 @@ class Game(arcade.Window):
 
         self.tex.image = self.im
         self.sp.texture = self.tex
+        self.clear()
         self.spl.draw()
 
     def on_key_press(self,symbol,modifiers):
